b898e1785edc27237be6bde31976551dab3a8852
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-08-22 13:26:16 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_065804) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "checked"
   end
 
 end
